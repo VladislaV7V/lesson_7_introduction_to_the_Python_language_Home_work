9e219810f6e9134b311f83c420fa7a5b5f443593
Vladislav <Vladislav19990330@yandex.ru>
2023-09-16 10:55:46 +0300

    Ex_36_v01

--- Ex_36/Ex_36.py
+++ Ex_36/Ex_36.py
@@ -20,3 +20,32 @@ print_operation_table(operation, num_rows=6, num_columns=6),
 4 8 12 16 20 24
 5 10 15 20 25 30
 6 12 18 24 30 36    """
+
+
+def copy_list(list_1):
+    list_2 = []
+    for i in list_1:
+        list_2.append(i)
+    return list_2
+
+def f(list_y, x, cou):
+    res_lis = []
+    res_lis.append(list_y[cou])
+    for _ in range(x-1):
+        res_lis.append(res_lis[-1] + res_lis[0])
+    return res_lis
+
+x = (int(input("Введите x: ")))
+y = (int(input("Введите y: ")))
+
+list_name = ["list_" + str(i) for i in range(1, y+1)]
+list_data = copy_list(list_name)
+
+list_data[0] = [n+1 for n in range(x)]
+list_y_nums = [m+1 for m in range(y)]
+
+for i in range(1, len(list_data)):
+    list_data[i] = f(list_y_nums, x, i)
+
+for i in range(len(list_name)):
+    print(f"{list_name[i]} - {list_data[i]}")
