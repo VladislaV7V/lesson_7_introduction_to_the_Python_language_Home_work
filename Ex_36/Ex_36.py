4c8aa8ba4fa8c7007ee9d10be4d3b5013a8c1352
Vladislav <Vladislav19990330@yandex.ru>
2023-09-16 11:19:39 +0300

    Ex_36_v02

--- Ex_36/Ex_36.py
+++ Ex_36/Ex_36.py
@@ -22,30 +22,29 @@ print_operation_table(operation, num_rows=6, num_columns=6),
 6 12 18 24 30 36    """
 
 
-def copy_list(list_1):
+def get_clean_list(list_1):
     list_2 = []
-    for i in list_1:
-        list_2.append(i)
+    for _ in list_1:
+        list_2.append([])
     return list_2
 
-def f(list_y, x, cou):
-    res_lis = []
-    res_lis.append(list_y[cou])
+def get_res_line(list_y_values, x, num_now_list):
+    res_list = []
+    res_list.append(list_y_values[num_now_list])
     for _ in range(x-1):
-        res_lis.append(res_lis[-1] + res_lis[0])
-    return res_lis
+        res_list.append(res_list[-1] + res_list[0])
+    return res_list
 
 x = (int(input("Введите x: ")))
 y = (int(input("Введите y: ")))
 
 list_name = ["list_" + str(i) for i in range(1, y+1)]
-list_data = copy_list(list_name)
+list_data = get_clean_list(list_name)
 
-list_data[0] = [n+1 for n in range(x)]
-list_y_nums = [m+1 for m in range(y)]
+list_y_values = [m+1 for m in range(y)]
 
-for i in range(1, len(list_data)):
-    list_data[i] = f(list_y_nums, x, i)
+for i in range(len(list_data)):
+    list_data[i] = get_res_line(list_y_values, x, i)
 
 for i in range(len(list_name)):
     print(f"{list_name[i]} - {list_data[i]}")
